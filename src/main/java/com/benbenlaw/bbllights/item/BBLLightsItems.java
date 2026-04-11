3105efb72f79512aba241438128b66c8fa3eae7e
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-04-11 09:55:45 +0100

    2.4.2

--- src/main/java/com/benbenlaw/bbllights/item/BBLLightsItems.java
+++ src/main/java/com/benbenlaw/bbllights/item/BBLLightsItems.java
@@ -10,16 +10,16 @@ public class BBLLightsItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BBLLights.MOD_ID);
 
     public static final DeferredItem<Item> INVISIBLE_LIGHT = ITEMS.registerItem("invisible_light",
-            LightItem::new, new Item.Properties());
+            properties -> new LightItem(new Item.Properties()));
 
     public static final DeferredItem<Item> IRON_INVISIBLE_LIGHT_PLACER = ITEMS.registerItem("iron_invisible_light_placer",
-            LightItem::new, new Item.Properties().durability(121));
+            properties -> new LightItem(new Item.Properties().durability(121)));
 
     public static final DeferredItem<Item> DIAMOND_INVISIBLE_LIGHT_PLACER = ITEMS.registerItem("diamond_invisible_light_placer",
-            LightItem::new, new Item.Properties().durability(481));
+            properties -> new LightItem(new Item.Properties().durability(418)));
 
     public static final DeferredItem<Item> NETHERITE_INVISIBLE_LIGHT_PLACER = ITEMS.registerItem("netherite_invisible_light_placer",
-            LightItem::new, new Item.Properties().durability(961));
+            properties -> new LightItem(new Item.Properties().durability(961)));
 
 }
 
